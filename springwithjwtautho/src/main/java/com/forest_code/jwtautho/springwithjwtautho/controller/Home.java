2e7534550ff24bc69377599489c713ba7ade2ca9
Bhubanananda2020 <bkhadi0203@gmail.com>
2021-10-17 17:14:47 +0530

    1

--- springwithjwtautho/src/main/java/com/forest_code/jwtautho/springwithjwtautho/controller/Home.java
+++ springwithjwtautho/src/main/java/com/forest_code/jwtautho/springwithjwtautho/controller/Home.java
@@ -1,9 +1,11 @@
 package com.forest_code.jwtautho.springwithjwtautho.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Home {
 
 	@RequestMapping("/welcome")
--- springwithjwtautho/src/main/java/com/forest_code/jwtautho/springwithjwtautho/controller/JwtController.java
+++ springwithjwtautho/src/main/java/com/forest_code/jwtautho/springwithjwtautho/controller/JwtController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import com.forest_code.jwtautho.springwithjwtautho.model.JwtResponse;
 import com.forest_code.jwtautho.springwithjwtautho.services.CustomUserDetailsService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JwtController {
 
 	@Autowired
